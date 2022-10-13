68381a3a47918f586d591083b3e99bd4339658c0
Adem Özdemir <61504080+4demozdemir@users.noreply.github.com>
2022-10-13 14:11:02 +0300

    solution one

---
+++ Apps/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using DataStructures.LinkedList.SinglyLinkedList;
+namespace Apps
+{
+    class Program
+    {
+            public static void Main(string[] args)
+            {
+        
+                UsingLinkedList();
+
+                var list = new LinkedList<int>();
+                list.AddFirst(1);
+                list.AddFirst(2);
+                list.AddFirst(3);
+                list.AddLast(4);
+
+                foreach (int item in list)
+                {
+                    Console.WriteLine(item);
+                }
+
+            }
+
+            private static void UsingLinkedList()
+            {
+                var linkedList = new SinglyLinkedList<int>();
+                linkedList.AddFirst(1);
+                linkedList.AddFirst(2);
+                linkedList.AddFirst(5);
+
+                linkedList.AddLast(4);
+                linkedList.AddLast(6);
+                linkedList.AddLast(7);
+
+                linkedList.AddAfter(linkedList.Head.Next, 21);
+            }
+
+
+
+
+
+        }
+    }
+
