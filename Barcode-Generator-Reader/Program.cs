1d581bc5d09fb5d28c9658e23267b1f817318a52
Adem Özdemir <61504080+4demozdemir@users.noreply.github.com>
2022-10-13 14:18:14 +0300

    project 3

---
+++ Barcode-Generator-Reader/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Barcode_Generator_Reader
+{
+    public class Program
+    {
+
+        // Barcode Generator/Reader
+        //3rd party barcode kütüphaneleri kullanarak barcode üreten bunu client'ın bilgisayarında bir lokasyona kaydeden. 
+        //Daha sonra da barcode'u okuyabilen bir console uygulaması yazınız.
+        //Kullanılması gereken teknikler:
+        //-----------------------------------------
+        //+Console Application
+        //+Import 3rd Party Library
+        //+Dosyaya Yazma
+        //+Dosyadan Okuma
+        
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
